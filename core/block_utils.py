26a40f1664a46dc1bfd0231a5f83787603e63375
Sa Phi <great_bn@yahoo.com>
2018-03-29 13:14:09 +0700

    add utils for block

---
+++ core/block_utils.py
@@ -0,0 +1,57 @@
+from config import *
+import datetime
+import block
+
+def is_valid_chain(blockchain):
+    """
+    Check entire chain is valid
+    """
+    for block in blockchain:
+        if not block.is_valid():
+            return False
+    return True
+
+
+def dict_from_block_attributes(**kwargs):
+    info = {}
+    for key in kwargs:
+        if key in BLOCK_VAR_CONVERSIONS:
+            info[key] = BLOCK_VAR_CONVERSIONS[key](kwargs[key])
+        else:
+            info[key] = kwargs[key]
+    
+    return info
+
+
+def create_new_block_from_prev(prev_block=None, data=None, timestamp=None):
+    if not prev_block:
+        index = 0
+        prev_hash = ''
+    else:
+        index = int(prev_block.index) + 1
+        prev_hash = prev_block.hash
+
+    if not timestamp:
+        timestamp = datetime.datetime.utcnow().strftime("%Y/%m/%d-%H:%M:%S:%f")
+    
+    nonce = 0
+    block_info_dict = dict_from_block_attributes(
+        index=index,
+        timestamp=timestamp,
+        data=data,
+        prev_hash=prev_hash,
+        nonce=nonce)
+    new_block = block.Block(block_info_dict)
+    return new_block
+
+
+def proof_of_work(find_block, data=None):
+    find_block.nonce = 0
+    find_block.update_self_hash()
+    if not find_block.data:
+        find_block.data = data
+    while str(find_block.hash[0:NUM_ZEROS]) != '0' * NUM_ZEROS:
+        find_block.nonce += 1
+        find_block.update_self_hash()
+    assert find_block.is_valid()
+    return find_block
